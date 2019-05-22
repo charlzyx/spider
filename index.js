b6822bc367e893e8bab02c9094c780a5505bd815
xiaochao.yang <xiaochao.yang@bianlifeng.com>
2019-05-22 23:54:43 +0800

    多tab

--- index.js
+++ index.js
@@ -18,57 +18,24 @@ const selectors = {
   backBtn: '#content > div > div > table:nth-child(2) > tbody > tr > td',
 };
 const config = {
-  fileName: null,
-  bootTime: new Date().toString(),
-  rebootTime: null,
-  browser: null,
-  page: null,
+  bootTime: null,
+  pages: [
+    // { fileName, page, startPage, endPage, currentPage, currentIndex },
+  ],
   website: 'http://app1.sfda.gov.cn/datasearch/face3/base.jsp?tableId=120&tableName=TABLE120&title=%CA%B3%C6%B7%C9%FA%B2%FA%D0%ED%BF%C9%BB%F1%D6%A4%C6%F3%D2%B5(SC)&bcId=145275419693611287728573704379',
-  // 开始页码
-  startPage: 1,
-  // 当前页码
-  currentPage: 1,
-  // 当前正在处理的列
-  currentIndex: 0,
-  // 结束页码
-  endPage: 100,
 };
 
-const errorHandler = async (e) => {
-  if (config.page) {
-    await config.page.close();
-  }
-  if (config.browser) {
-    await config.browser.close();
-  }
-  const data = { message: e.message, stack: e.stack, time: new Date().toString() };
-  fs.appendFile(logPath, JSON.stringify(data, null, 2), { encoding: 'utf-8' }, (error) => { });
-  console.log('-------------------------------------');
-  console.log('出错啦!正在自动重启...');
-  console.log(`--当前时间:${new Date().toString()}----`);
-  console.log(`--:${config.startPage}:${config.startPage}:${config.endPage}----`);
-  console.log(`--下一页:${config.currentPage}----`);
-  console.log(`--正在处理列:${config.currentIndex}----`);
-  console.log(`--结束页:${config.endPage}----`);
-  console.log('-------------------------------------');
-  boot(config.startPage, config.endPage, config.currentPage)
+const errorHandler = async (e, pageConfig) => {
+  const { pageRef, lastPage, currentPage, index } = pageConfig;
+  const info = { page: index, time: new Date(), message: e.message, stack: e.stack, config: { ...pageConfig, pageRef: null } };
+  await fs.appendFileSync(logPath, `${new Date()}--${JSON.stringify(info, null, 2)}\n`);
+  pageConfig.currentPage = lastPage || currentPage;
+  // 休息1s再继续
+  await pageRef.waitFor(1000);
+  return begin(pageRef, pageConfig);
 }
 
-const typePage = async (to, page) => {
-  await page.tap(selectors.goInput);
-  await page.click(selectors.goInput, { clickCount: 3 });
-  await page.keyboard.press("Backspace");
-  await page.type(selectors.goInput, `${to}`);
-  await page.click(selectors.goBtn);
-}
-/**
- * 从1开始
- */
-const jumpTo = async (to, page) => {
-  await typePage(to, page);
-  await page.waitForResponse(resp => /.+\.jsp/.test(resp.url()));
-  await page.waitFor(200);
-}
+
 
 const pageParser = async (page) => {
   // 第 1 页 共10508页 共157614条
@@ -80,7 +47,8 @@ const pageParser = async (page) => {
   return { pageSize, pageNo, pageCount, dataCount };
 }
 
-const feeder = async (a, page) => {
+const got = async (a, page, conf) => {
+  const { fileName, index } = conf;
   try {
     const innerText = await a.getProperty('innerText');
     const id = /\d+/.exec(innerText)[0];
@@ -90,71 +58,118 @@ const feeder = async (a, page) => {
     // 给一点点渲染时间
     await page.waitFor(200);
     const info = await page.$$eval(selectors.info, tds => {
-      return tds.map(td => td.innerText.replace(/\,/g, '__comma__').replace(/\n/g, ';;;'));
+      return tds.map(td => td.innerText.replace(/\,/g, '__comma__').replace(/\n/g, '__enter__'));
     });
     info.unshift(id);
-    await fs.appendFileSync(config.fileName, info.join(','), { encoding: 'utf-8' });
-    await fs.appendFileSync(config.fileName, '\n', { encoding: 'utf-8' });
-    console.log(`${id}---${name}`);
+    await fs.appendFileSync(fileName, info.join(','), { encoding: 'utf-8' });
+    await fs.appendFileSync(fileName, '\n', { encoding: 'utf-8' });
+    console.log(`${new Date()} [${index}]-${id}---${name}\n`);
+    await fs.appendFileSync(logPath, `[${index}]-${id}---${name}\n`);
     await page.click(selectors.backBtn);
     await page.waitFor(500);
     return true;
   } catch (e) {
-    errorHandler(e);
+    return errorHandler(e, conf);
   }
 };
 
-const loopAList = async (page, startPage, endPage) => {
-  const list = await page.$$(selectors.alist);
-  const len = list.length;
-  let index = config.currentIndex;
-  while (index < len) {
-    const alist = await page.$$(selectors.alist);
-    await feeder(alist[index], page);
-    config.currentIndex = index;
-    index++;
+const typePage = async (to, page) => {
+  await page.tap(selectors.goInput);
+  await page.click(selectors.goInput, { clickCount: 3 });
+  await page.keyboard.press("Backspace");
+  await page.type(selectors.goInput, `${to}`);
+  await page.click(selectors.goBtn);
+}
+
+const jumpTo = async (page, conf) => {
+  try {
+    const { currentPage } = conf;
+    await typePage(currentPage, page);
+    await page.waitForResponse(resp => /.+\.jsp/.test(resp.url()));
+    await page.waitFor(300);
+  } catch (e) {
+    return errorHandler(e, conf);
   }
-  config.currentIndex = 0;
-  config.currentPage++;
-  if (config.currentPage <= endPage) {
-    await jumpTo(config.currentPage, page);
-    console.log('-------------------------------------');
-    console.log(`--启动时间:${config.bootTime}----`);
-    console.log(`--当前时间:${new Date().toString()}----`);
-    console.log(`--${config.currentPage}-${config.endPage}---`);
-    console.log('-------------------------------------');
-    return loopAList(page, startPage, endPage);
+}
+
+const loopAList = async (page, conf) => {
+  const { startPage, endPage, currentPage, lastPage, lastIndex, index } = conf;
+
+  const list = await page.$$(selectors.alist);
+
+  const len = list.length;
+  let i = lastIndex || 0;
+
+  while (i < len) {
+    const alist = await page.$$(selectors.alist);
+
+    await got(alist[i], page, conf);
+
+    conf.lastIndex = i;
+    i++;
+  }
+
+  conf.lastIndex = 0;
+  conf.currentPage = currentPage + 1;
+  conf.lastPage = currentPage;
+
+  config.pages[index] = conf;
+
+  if (conf.currentPage <= endPage) {
+    try {
+      await jumpTo(page, conf);
+      return loopAList(page, conf);
+    } catch (e) {
+      return errorHandler(e, conf);
+    }
   } else {
     return true;
   }
 };
 
-var boot = async (startPage = 1, endPage = 10, currentPage) => {
-  if (!currentPage) {
-    config.fileName = filePath(startPage, endPage);
-  }
-  console.log('-------------------------------------');
-  console.log('- spider start! powered by puppeteer-');
-  console.log('-------------------------------------');
-  config.rebootTime = new Date().toString();
-  config.startPage = currentPage || startPage;
-  config.currentPage = currentPage || startPage;
-  config.endPage = endPage;
-  const browser = config.browser = await pptr.launch({ headless: false });
-  const page = config.page = await browser.newPage();
-  page.setViewport({ width: 1280, height: 1400 });
+const begin = async (page, conf) => {
   await page.goto(config.website, { waitUntil: 'networkidle0' });
-  await jumpTo(config.startPage, page);
-  const { pageSize, pageNo, pageCount, dataCount } = await pageParser(page);
-  await loopAList(page, config.startPage, config.endPage);
-  await config.page.close();
-  await config.browser.close();
+  await page.waitFor(500);
+  await jumpTo(page, conf);
+  await loopAList(page, conf);
+};
+
+var boot = async (conf = {}) => {
+  config.bootTime = new Date();
+  const { startPage = 1, endPage = 10, tabCount = 3 } = conf;
+  const browser = await pptr.launch({ headless: true });
+  const pages = [];
+  let pageIndex = 0;
+  while (pageIndex < tabCount) {
+    const page = await browser.newPage();
+    page.setViewport({ width: 1280, height: 1400 });
+    pages[pageIndex] = page;
+    pageIndex++;
+  }
+
+  const per = Math.ceil((endPage - startPage + 1) / tabCount);
+  const pagesConfig = config.pages = pages.map((p, index) => {
+    const start = startPage + per * index;
+    const end = Math.min(startPage + per * (index + 1) - 1, endPage);
+    return {
+      index,
+      fileName: filePath(start, end),
+      pageRef: pages[index],
+      bootTime: new Date().toLocaleString(),
+      startPage: start,
+      endPage: end,
+      currentPage: start,
+    }
+  });
+
+  await Promise.all(pages.map((page, index) => begin(page, pagesConfig[index])));
   console.log('-------------------------------------');
   console.log('ALL DONE!');
-  console.log(`--启动时间:${config.bootTime}----`);
-  console.log(`--当前时间:${new Date().toString()}----`);
-  console.log(`--花费时间:${(+new Date() - +new Date(config.bootTime)) / ONEMIN}分钟----`);
+  console.log(`--${config.bootTime}--${new Date().toString()}--`);
+  console.log(`--${(+new Date() - +new Date(config.bootTime)) / ONEMIN} min----`);
   console.log('-------------------------------------');
-};
+  browser.close();
+  process.exit();
+}
 
 boot();
